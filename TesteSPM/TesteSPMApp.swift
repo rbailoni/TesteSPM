50111a7e73acbbaec044829ed88dfb6b43b53d63
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2024-06-06 22:50:23 -0300

    Initial Commit

---
+++ TesteSPM/TesteSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteSPMApp.swift
+//  TesteSPM
+//
+//  Created by Ricardo Bailoni on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TesteSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
