8550b0048718213dbefd00965d1cd303ff33c455
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2024-08-25 16:51:17 -0300

    Versão para testes

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '15.0'
+
+target 'TesteSPM' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Core2', :git => 'https://github.com/rbailoni/Core2.git', :tag => '0.1.4'
+
+end
--- TesteSPM/ContentView.swift
+++ TesteSPM/ContentView.swift
@@ -6,19 +6,89 @@
 //
 
 import SwiftUI
+import ViewCodeProtocols
+import NetworkProtocols
 
 struct ContentView: View {
+    @State var users: [User] = []
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(users) { user in
+                Text(user.name)
+            }
         }
         .padding()
+        .task {
+            do {
+                users = try await APIManager.shared.getData(from: APIEndPoint.users)
+                let carambola = JSONBody()
+            } catch {
+                
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+enum APIEndPoint: EndPointProtocol {
+    
+    case users
+    
+    var baseURL: String {
+        guard let base = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String else { return "" }
+        return base
+    }
+    
+    var path: String {
+        switch self {
+        case .users: "/users"
+        }
+    }
+    
+    var method: NetworkProtocols.Method {
+        switch self {
+        default: .GET
+        }
+    }
+    
+    var queries: [URLQueryItem]? {
+        switch self {
+        default: nil
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .users:
+            nil
+        }
+    }
+}
+
+struct User: Codable, Identifiable {
+    let id: Int?
+    let name: String
+    let username, email: String?
+    let address: Address?
+    let phone, website: String?
+    let company: Company?
+}
+
+// MARK: - Address
+struct Address: Codable {
+    let street, suite, city, zipcode: String?
+    let geo: Geo?
+}
+
+// MARK: - Geo
+struct Geo: Codable {
+    let lat, lng: String?
+}
+
+// MARK: - Company
+struct Company: Codable {
+    let name, catchPhrase, bs: String?
 }
